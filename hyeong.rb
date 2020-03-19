8c2b44ea0762703547d40d1de6db8f7957c447a4
buttercrab <jaeyong0201@gmail.com>
2020-03-19 21:02:28 +0900

    updated version

--- hyeong.rb
+++ hyeong.rb
@@ -1,14 +1,14 @@
 class Hyeong < Formula
-  version '0.1.0-beta.2'
+  version '0.1.0'
   desc "Hyeo-ung Programming Language Compiler in Rust (beta release)"
   homepage "https://github.com/buttercrab/hyeo-ung-lang"
 
   if OS.mac?
-    url "https://github.com/buttercrab/hyeo-ung-lang/releases/download/v#{version}/hyeong-darwin-x86-64.tar.gz"
-    sha256 "b8c3cc5271503effaaef358d096bdb57d6481af0cee7e6656eb2b0c12f704d5e"
+    url "https://github.com/buttercrab/hyeo-ung-lang/releases/download/v#{version}/hyeong-#{version}-darwin-x86-64.tar.gz"
+    sha256 "24990f87afe0813a0d67ba6525bc822b73f470c58692b5eedcdb5203dec2aa63"
   elsif OS.linux?
-    url "https://github.com/buttercrab/hyeo-ung-lang/releases/download/v#{version}/hyeong-linux-x86-64.tar.gz"
-    sha256 "f63b9df26b964292fd7abfa47cec897080d982f6d2167d3dc25ecbedc07e6a30"
+    url "https://github.com/buttercrab/hyeo-ung-lang/releases/download/v#{version}/hyeong-#{version}-linux-x86-64.tar.gz"
+    sha256 "82ede138f347af3034a4c2b3efd84393ec60acd33af77f209cd59e4afb74fe98"
   end
 
   def install
