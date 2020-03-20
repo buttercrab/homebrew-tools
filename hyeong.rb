f6e1ec11eacbefdfc8c3d19fe5e91e732b218a24
buttercrab <jaeyong0201@gmail.com>
2020-03-20 13:51:30 +0900

    hyeong 0.1.0

--- hyeong.rb
+++ hyeong.rb
@@ -13,5 +13,14 @@ class Hyeong < Formula
 
   test do
     assert_match "hyeong 0.1.0", shell_output("#{bin}/hyeong --version")
+    hello_world = File.new("hello_world.hyeong", "w")
+    hello_world.syswrite "혀어어어어어어어엉........ 핫. 혀엉..... 흑... 하앗...
+                          흐윽... 형.  하앙.혀엉.... 하앙... 흐윽... 항. 항. 형...
+                          하앙. 흐으윽... 형... 흡... 혀엉..하아아앗. 혀엉.. 흡...
+                          흐읍... 형.. 하앗. 하아앙... 형... 하앙... 흐윽...혀어어엉..
+                          하앙. 항. 형... 하앙. 혀엉.... 하앙. 흑... 항. 형... 흡  하앗.
+                          혀엉..... 흑. 흣"
+    hello_world.close
+    assert_match "Hello, world!\n", shell_output("#{bin}/hyeong intall;#{bin}/hyeong build hello_world.hyeong;./hello_world")
   end
 end
